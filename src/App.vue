f01dc9f066fc774d803ca3855ae9511a6d244bfd
sam <204447118@qq.com>
2019-07-04 00:31:57 +0800

    初步搭建

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
+  },
+  created() {
+    console.log('process.env:<')
+    console.log(process.env)
+    console.log('>')
   }
 }
 </script>
